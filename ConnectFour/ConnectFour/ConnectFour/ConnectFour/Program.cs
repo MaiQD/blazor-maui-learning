a223305273cfe5dd99738f52eab57cee7b4b586e
datmai <q.dat2710@gmail.com>
2026-01-02 02:02:55 +0700

    Add initial game logic and UI components for Connect Four

---
+++ ConnectFour/ConnectFour/ConnectFour/ConnectFour/Program.cs
@@ -0,0 +1,37 @@
+using ConnectFour.Client.Pages;
+using ConnectFour.Components;
+using ConnectFour.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddSingleton<GameState>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(ConnectFour.Client._Imports).Assembly);
+
+app.Run();
